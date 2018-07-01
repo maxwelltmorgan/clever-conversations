d658acf52aca98fa63f673a5557a386cd0e54107
Maxwell Morgan <maxwelltmorgan@gmail.com>
2018-06-30 23:32:36 -0600

    review

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import * as firebase from 'firebase';
-//import { PageHeader } from 'react-bootstrap';
 import { RoomList } from './components/RoomList.js';
 import { MessageList } from './components/MessageList.js';
 import { User } from './components/User.js';
